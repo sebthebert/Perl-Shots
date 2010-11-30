29ed5238f8f110630b060c29a5843e22a2d2c208
Sebastien Thebert <sebthebert@gmail.com>
2010-12-01 00:42:44 +0100

    First commit

---
+++ Benchmarks/strings.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl
+
+=head1 DESCRIPTION
+
+Benchmark related to StackOverflow question: http://stackoverflow.com/q/3104493/24820
+
+=cut
+
+use strict;
+use warnings;
+
+use Benchmark qw(cmpthese timethese);
+
+my $bench = timethese(
+    $ARGV[1],
+    {
+
+        multi_concat => sub {
+            my $string =
+                "Hi, I am a very long and chatty string that just won't";
+            $string .= " quit.  I'm going to keep going, and going, and going,";
+            $string .=
+                " kind of like the Energizer bunny.  What are you going to";
+            $string .= " do about it?";
+        },
+
+        one_concat => sub {
+            my $string =
+                  "Hi, I am a very long and chatty string that just won't"
+                . " quit.  I'm going to keep going, and going, and going,"
+                . " kind of like the Energizer bunny.  What are you going to"
+                . " do about it?";
+        },
+
+        join => sub {
+            my $string = join('',
+                "Hi, I am a very long and chatty string that just won't",
+                " quit.  I'm going to keep going, and going, and going,",
+                " kind of like the Energizer bunny.  What are you going to",
+                " do about it?");
+        },
+
+    }
+);
+
+cmpthese $bench;
+
+1;
+
+=head1 AUTHOR
+
+Sebastien Thebert <sebthebert@gmail.com>
+
+=cut
